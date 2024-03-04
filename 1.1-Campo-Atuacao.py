98edbecf37f7134fcfa04feec82020b4e010fb25
HernaldoMeneses <hernaldomeneses@gmail.com>
2024-03-03 21:13:48 -0300

    1.1-Campo-Aruacao.py

---
+++ 1.1-Campo-Atuacao.py
@@ -0,0 +1,17 @@
+Contabilidade = '''
+    Ciêncis Social aplicada
+        Tecnicas e metodologias para
+            Capta, registra, acumular, resumir e interpretar 
+                fenômenos 
+                    patrimoniais, financeiros e econômicos 
+                        das Entidades
+                            Com e Sem fins lucrativos
+                                Pessoas físicas
+                                Pessoas Jurídicas
+                                    D.Público
+                                        Estado, Município, União, Autarquia
+                                    Privado
+
+'''
+
+#pg. 15/345
