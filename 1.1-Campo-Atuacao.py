f6b193d54d8a56205bddf96be88494c1d1b0e6bd
HernaldoMeneses <140676491+HernaldoMeneses@users.noreply.github.com>
2024-03-08 12:04:57 -0300

    1.1-campo-atuacao.py

--- 1.1-Campo-Atuacao.py
+++ 1.1-Campo-Atuacao.py
@@ -13,5 +13,53 @@ Contabilidade = '''
                                     Privado
 
 '''
+class Topico:
+    def __init__(self, tema, tese, solution, brain_storm, frases):
+        self.tema = tema
+        self.tese = tese
+        self.solution = solution
+        self.brain_storm = brain_storm
+        self.frases = frases
 
+    def definir_tema(self, new_tema):
+        self.tema = new_tema
+
+    def definir_tese(self, new_tese):
+        self.tese = new_tese
+    
+    def definir_solution(self, new_solution):
+        self.solution = new_solution
+    
+    def definir_brain_srotm(self, new_brain_srotm):
+        self.brain_storm = new_brain_srotm
+
+    def definir_brain_srotm(self, new_frases):
+        self.frases = new_frases
+
+    def imprimir_informacoes(self):
+        print(f"Tema: {self.tema}, \n Tese: {self.tese}")
+        print(f"Solution: {self.solution}")
+        print(f"Brain_Storm: {self.brain_storm}")
+        print(f"Frases: {self.frases}")
+        cout =1
+        for i in (self.frases):
+            print(f"Frase: {cout} {i}")
+            cout = cout +1
+
+# Exemplo de uso da classe
+tema_ = 'Tema - Campo de atuação da Contabilidade'
+tese_ = 'Tese - '
+solution_ = 'Solution - '
+brain_storm_ = [
+    'Word1','Word2']
+frases_ = [
+    'frase1','frase2']
+topic1 = Topico(
+    tema=tema_, 
+    tese=tese_, 
+    solution=solution_, 
+    brain_storm=brain_storm_,
+    frases=frases_)
+topic1.imprimir_informacoes()
 #pg. 15/345
+#file:///L:/Linear_/Base-Jump_08/1%20-%20Contabilidade%20Introdut%C3%B3ria%20(Livro%20Texto)%20-%20Sergio%20Iudicibus.pdf
---
+++ r.cmd
@@ -0,0 +1,3 @@
+@echo off 
+python 1.1-campo-atuacao.py 
+cmd
